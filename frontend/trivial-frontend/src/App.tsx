bd03395a479d66ecb686a4333cb7ac62f8047f45
shwballl <halingriz@gmail.com>
2025-04-13 23:32:47 +0300

    Inialize React App with Tailwind CSS.

---
+++ frontend/trivial-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-600 text-white text-3xl">
+      Tailwind работает! 🚀
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/trivial-frontend/src/index.tsx
@@ -0,0 +1,12 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  // <React.StrictMode> // временно закомментируй
+    <App />
+  // </React.StrictMode>
+);
